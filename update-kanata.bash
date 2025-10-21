1654840bd89f2a2c7cd8a3468055e18419a239f1
gooberlogic <gooberlogic@proton.me>
2025-10-21 16:41:01 -0400

    prevent accidental kbd deletion

--- update-kanata.bash
+++ update-kanata.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rm kanata*
+rm kanata.exe kanata
 
 wget https://github.com/jtroo/kanata/releases/latest/download/kanata_winIOv2.exe -O kanata.exe
 wget https://github.com/jtroo/kanata/releases/latest/download/kanata
